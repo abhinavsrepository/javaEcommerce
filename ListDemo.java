69356edb60609875d9d15f14963dd10386f0f411
abhinavsrepository <abhi2510979b@gmail.com>
2025-06-07 23:51:04 +0530

    initial commit

---
+++ ListDemo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class ListDemo
+{
+    public static void main(String[]args)
+    {
+        ArrayList<Integer> al1 = new ArrayList<>(20);
+
+        ArrayList<Integer> al2 = new ArrayList<>(List.of(50,60,70,80,90));
+        all.add(10);
+        all.add(0,5);
+        al1.addAll(al2);
+        System.out.println(al1);
+
+
+
+
+    }
+}
