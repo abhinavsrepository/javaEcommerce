a5d377dcbf9525b1ccc5b3311b9399cab936d00d
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2025-06-12 00:00:04 +0530

    Update README.md

--- src/main/java/com/mvhgroup/domain/USER_ROLE.java
+++
@@ -1,9 +0,0 @@
-package com.mvhgroup.domain;
-
-public enum USER_ROLE {
-    ROLE_ADMIN,
-    ROLE_CUSTOMER,
-    ROLE_USER,
-
-
-}
--- src/main/java/com/mvhgroup/response/ApiResponse.java
+++
@@ -1,9 +0,0 @@
-package com.mvhgroup.response;
-
-import lombok.Data;
-
-@Data
-public class ApiResponse {
-    private String message;
-
-}
